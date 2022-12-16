39248069da59d84be6b0565818800d0092a800fa
hrantMes <hrant.meselyan.g@tumo.org>
2022-12-16 23:23:15 +0400

    homework

---
+++ src/group2/example.java
@@ -0,0 +1,33 @@
+package group2;
+
+public class example {
+    public static void main(String[] args) {
+        for (int i = 1; i < 6; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 4; k > i; k--) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+
+    }
+}
