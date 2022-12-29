ed0ad9d9ee9df0e8c83fe34b52d531932c2f9b36
dell <hrant3307@gmail.com>
2022-12-29 11:28:31 -0800

    homework

--- src/homework/homework4/CharArrayExample.java
+++ src/homework/homework4/CharArrayExample.java
@@ -3,7 +3,7 @@ package homework.homework4;
 public class CharArrayExample {
     public static void main(String[] args) {
 
-        char[] chars = {'j', 'a', 'v', 'a', 'l', 'o', 'v', 'e', 'o' };
+        char[] chars = {'j', 'a', 'v', 'a', 'l', 'o', 'v', 'e', 'o'};
         int count = 0;
         char c = 'o';
         for (int x : chars) {
@@ -14,25 +14,21 @@ public class CharArrayExample {
         System.out.println(count);
 
 
-        char[] chars2 = {'j', 'a', 'v', 'a', 'l', 'o', 'v', 'e' };
+        char[] chars2 = {'j', 'a', 'v', 'a', 'l', 'o', 'v', 'e'};
         System.out.println(chars2[chars2.length / 2] + " " + chars2[(chars2.length / 2) - 1]);
 
 
-        char[] chars3 = {'j', 'a', 'v', 'a', 'l', 'o', 'l', 'y' };
-        if (chars3[chars3.length - 1] == 'y' && chars3[chars3.length - 2] == 'l') {
-            System.out.println("true");
-        } else {
-            System.out.println("false");
-        }
+        char[] chars3 = {'j', 'a', 'v', 'a', 'l', 'o', 'l', 'y'};
+        System.out.println(chars3[chars3.length - 1] == 'y' && chars3[chars3.length - 2] == 'l');
 
 
-        char[] text = {' ', ' ', 'b', 'a', 'r', 'e', 'v', ' ', ' ' };
+        char[] text = {' ', ' ', 'b', 'a', 'r', 'e', 'v', ' ', ' '};
         for (char x : text) {
-           if (x == ' '){
-               continue;
-           }else {
-               System.out.print(x + " ");
-           }
+            if (x != ' ') {
+                System.out.print(x + " ");
+            }
         }
+
+
     }
 }
