70e172122c13200ddf1db72ae34662b14c9e128d
dell <hrant3307@gmail.com>
2023-01-14 03:33:33 -0800

    homework7

---
+++ src/homework/homework7/Calculator.java
@@ -0,0 +1,29 @@
+package homework.homework7;
+
+public class Calculator {
+    int firstNumber;
+    int secondNumber;
+
+
+    int plus(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    int minus(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    int divide(int firstNumber, int secondNumber) {
+        if (secondNumber == 0) {
+            System.out.println("is not divisible by 0");
+            return -1;
+        }
+        return firstNumber / secondNumber;
+
+    }
+
+    int multiply(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+}
+
---
+++ src/homework/homework7/CalculatorDemo.java
@@ -0,0 +1,19 @@
+package homework.homework7;
+
+public class CalculatorDemo {
+    public static void main(String[] args) {
+        Calculator result = new Calculator();
+
+        int sum = result.plus(10, 5);
+        System.out.println("plus : " + sum);
+
+        sum = result.minus(6, 99);
+        System.out.println("minus : " + sum);
+
+        sum = result.divide(900, 0);
+        System.out.println("divide : " + sum);
+
+        sum = result.multiply(80, 2);
+        System.out.println("multiply : " + sum);
+    }
+}
