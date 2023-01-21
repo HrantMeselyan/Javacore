f629a7ef56c478800b7508216dd6ea99e7e019bb
dell <hrant3307@gmail.com>
2023-01-21 11:51:22 -0800

    HomeWork9

--- src/homework/homework8/DynamicArrayDemo.java
+++ src/homework/homework8/DynamicArrayDemo.java
@@ -5,26 +5,31 @@ public class DynamicArrayDemo {
     public static void main(String[] args) {
         DynamicArray array = new DynamicArray();
 
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 30; i++) {
             array.add(i);
         }
+        System.out.print("Array : ");
         array.print();
         System.out.println();
 
-        System.out.println(array.get(0));
+        System.out.println("Get Element By Index : " + array.get(0));
 
+        System.out.print("Delete Element By Index : ");
         array.delete(8);
         System.out.println();
 
+        System.out.print("Set Element : ");
         array.set(1, 7);
-
         System.out.println();
-        array.add(1, 47);
 
+        System.out.print("Add Element : ");
+        array.add(2, 47);
         System.out.println();
+
+        System.out.print("Exists : ");
         System.out.println(array.exists(0));
 
-        System.out.println(array.getIndexByValue(1));
+        System.out.println("Get Value Index : " + array.getIndexByValue(47));
 
 
     }
