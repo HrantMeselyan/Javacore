625a9dae7be2d7ff8ccead984b88ff6bc1e14262
dell <hrant3307@gmail.com>
2023-01-23 02:47:32 -0800

    homework9 fix all bugs

--- src/homework/homework8/DynamicArray.java
+++ src/homework/homework8/DynamicArray.java
@@ -34,42 +34,39 @@ public class DynamicArray {
     }
 
     public void delete(int index) {
-        if (index < size && index >= 0) {
-            for (int i = index; i < size - 1; i++) {
-                array[i] = array[i + 1];
-            }
-            size--;
-            print();
-        } else {
+        if (index >= size || index < 0) {
             System.out.println("Index Not Found");
+            return;
         }
+        for (int i = index; i < size - 1; i++) {
+            array[i] = array[i + 1];
+        }
+        size--;
     }
+
 
     public void set(int index, int value) {
         if (index < 0 || index >= size) {
             System.out.println("Index Not Found");
-        } else {
-            array[index] = value;
-            print();
+            return;
         }
+        array[index] = value;
 
     }
 
     public void add(int index, int value) {
         if (index >= size || index < 0) {
             System.out.println("Index Not Found");
-        } else {
-            if (size == array.length) {
-                extend();
-                size++;
-            }
-            for (int i = size; i > index; i--) {
-                array[i] = array[i - 1];
-            }
-            array[index] = value;
-
-            print();
+            return;
         }
+        if (size  == array.length) {
+            extend();
+        }
+        for (int i = size; i > index; i--) {
+            array[i] = array[i - 1];
+        }
+        size++;
+        array[index] = value;
     }
 
     public boolean exists(int value) {
--- src/homework/homework8/DynamicArrayDemo.java
+++ src/homework/homework8/DynamicArrayDemo.java
@@ -15,21 +15,24 @@ public class DynamicArrayDemo {
         System.out.println("Get Element By Index : " + array.get(10));
 
         System.out.print("Delete Element By Index : ");
-        array.delete(4);
+        array.delete(5);
+        array.print();
         System.out.println();
 
         System.out.print("Set Element : ");
         array.set(0, 7);
+        array.print();
         System.out.println();
 
         System.out.print("Add Element : ");
-        array.add(2, 47);
+        array.add(8, 47);
+        array.print();
         System.out.println();
 
         System.out.print("Exists : ");
         System.out.println(array.exists(7));
 
-        System.out.println("Get Value Index : " + array.getIndexByValue(47));
+        System.out.println("Get Value Index : " + array.getIndexByValue(15));
 
     }
 }
