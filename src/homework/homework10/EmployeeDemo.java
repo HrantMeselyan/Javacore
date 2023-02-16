873df2dfddc69c1a98d14f3516e9b264b1da1a86
dell <hrant3307@gmail.com>
2023-02-16 08:27:56 -0800

    employee

---
+++ src/homework/homework10/EmployeeDemo.java
@@ -0,0 +1,130 @@
+package homework.homework10;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class EmployeeDemo {
+    private static Scanner scanner = new Scanner(System.in);
+    private static EmployeeStorage employeeStorage = new EmployeeStorage();
+
+    public static void main(String[] args) throws ParseException {
+        boolean isRun = true;
+        while (isRun) {
+            menu();
+            String command = scanner.nextLine();
+            switch (command) {
+                case "0":
+                    isRun = false;
+                    break;
+                case "1":
+                    employeeAdd();
+                    break;
+                case "2":
+                    employeeStorage.print();
+                    break;
+                case "3":
+                    System.out.println("Please input employee id");
+                    String idForSearch = scanner.nextLine();
+                    System.out.println(employeeStorage.search(idForSearch));
+                    break;
+                case "4":
+                    System.out.println("Please input company name");
+                    String companyNameSearch = scanner.nextLine();
+                    employeeStorage.searchByCompanyName(companyNameSearch);
+                    break;
+                case "5":
+                    searchByRange();
+                    break;
+                case "6":
+                    changeEmployeePosition();
+                    break;
+                case "7":
+                    employeeStorage.printActiveEmployees();
+                    break;
+                case "8":
+                    System.out.println("Please input id for inactive employee");
+                    String id = scanner.nextLine();
+                    employeeStorage.inactiveEmployeeById(id);
+                    break;
+                case "9":
+                    System.out.println("Please input id for activate employee");
+                    String employeeId = scanner.nextLine();
+                    employeeStorage.activateEmployeeById(employeeId);
+                    break;
+                default:
+                    System.out.println("Wrong command .Please try again");
+
+            }
+        }
+    }
+
+    private static void menu() {
+        System.out.println("Please input 0 for exit");
+        System.out.println("Please input 1 for add employee");
+        System.out.println("Please input 2 for print all employee");
+        System.out.println("Please inout 3 for search employee by employee id");
+        System.out.println("Please inout 4 for search employee by company name");
+        System.out.println("Please input 5 for search employee by salary range");
+        System.out.println("Please input 6 for change employee position by id");
+        System.out.println("Please input 7 for print only active employees");
+        System.out.println("Please input 8 for inactive employee by id");
+        System.out.println("Please input 9 for activate employee by id");
+    }
+
+
+    private static void employeeAdd() throws ParseException {
+        System.out.println("please input name");
+        String name = scanner.nextLine();
+
+        System.out.println("please input surname");
+        String surname = scanner.nextLine();
+
+        System.out.println("please input author id");
+        String id = scanner.nextLine();
+        if (!employeeStorage.checkID(id)) {
+            System.out.println("Wrong index");
+            return;
+        }
+        System.out.println("please input salary");
+        String salary = scanner.nextLine();
+
+        System.out.println("Please input company name");
+        String companyName = scanner.nextLine();
+
+        System.out.println("Please input position");
+        String position = scanner.nextLine();
+
+        Date date = new Date();
+        Employee employee = new Employee(name, surname, id, Double.parseDouble(salary), companyName, position, date, dateOfBirthday());
+
+        employeeStorage.add(employee);
+        System.out.println("Employee is added!");
+    }
+
+    private static void searchByRange() {
+        System.out.println("Please input minimum");
+        int min = Integer.parseInt(scanner.nextLine());
+        System.out.println("Please input maximum");
+        int max = Integer.parseInt(scanner.nextLine());
+        employeeStorage.searchByMaxAndMin(min, max);
+    }
+
+    private static void changeEmployeePosition() {
+        System.out.println("Please input id for change position");
+        String id = scanner.nextLine();
+        System.out.println("Please input position");
+        String position = scanner.nextLine();
+        employeeStorage.changeEmployeePositionById(id, position);
+        System.out.println("Position is changed");
+    }
+
+    private static Date dateOfBirthday() throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        System.out.println("Please input employee birthday (12/02/2023)");
+        String dateOfBirthdayStr = scanner.nextLine();
+        Date parse = sdf.parse(dateOfBirthdayStr);
+        return parse;
+    }
+}
---
+++ src/homework/homework10/EmployeeStorage.java
@@ -0,0 +1,118 @@
+package homework.homework10;
+
+
+public class EmployeeStorage {
+    private Employee[] array = new Employee[10];
+    private int size;
+
+    public void add(Employee item) {
+        if (size == array.length) {
+            extend();
+        }
+        array[size++] = item;
+    }
+
+
+    public void print() {
+        for (int i = 0; i < size; i++) {
+            System.out.println(array[i] + " ");
+        }
+    }
+
+
+    public void extend() {
+        Employee[] array2 = new Employee[array.length + 10];
+        System.arraycopy(array, 0, array2, 0, array.length);
+        array = array2;
+    }
+
+
+    public Employee search(String id) {
+        for (int i = 0; i < size; i++) {
+            if (array[i].getID().toLowerCase().contains(id.toLowerCase())) {
+                return (array[i]);
+            }
+        }
+        return null;
+    }
+
+    public boolean checkID(String id) {
+        boolean check = true;
+        for (int i = 0; i < size; i++) {
+            if (id.equals(array[i].getID())) {
+                check = false;
+                break;
+            }
+        }
+        return check;
+    }
+
+    public void searchByCompanyName(String companyNameSearch) {
+        boolean found = false;
+        for (int i = 0; i < size; i++) {
+            if (array[i].getCompany().toLowerCase().contains(companyNameSearch.toLowerCase())) {
+                found = true;
+                System.out.println(array[i].getName() + " " + array[i].getSurname());
+            }
+        }
+        if (!found) {
+            System.out.println("company " + companyNameSearch + "does not exits");
+        }
+    }
+
+    public void searchByMaxAndMin(int min, int max) {
+        for (int i = 0; i < size; i++) {
+            double salaryDouble = array[i].getSalary();
+            int salaryInt = (int) salaryDouble;
+            if (min <= salaryInt && max >= salaryInt)
+                System.out.println(array[i] + " ");
+        }
+    }
+
+    public void changeEmployeePositionById(String id, String position) {
+        for (int i = 0; i < size; i++) {
+            String getId = array[i].getID();
+            if (getId.equals(id)) {
+                array[i].setPosition(position);
+            }
+        }
+    }
+
+    public void printActiveEmployees() {
+        for (int i = 0; i < size; i++) {
+            if (array[i].isActive()) {
+                System.out.println(array[i] + " ");
+            }
+        }
+    }
+
+    public void inactiveEmployeeById(String id) {
+        for (int i = 0; i < size; i++) {
+            if (id.equals(array[i].getID()) && array[i].isActive()) {
+                array[i].setActive(false);
+                System.out.println("Status changed!");
+            } else if (id.equals(array[i].getID()) && !array[i].isActive()) {
+                System.out.println("Status is already false");
+                break;
+            } else {
+                System.out.println("Wrong id please try again");
+                return;
+            }
+        }
+    }
+
+    public void activateEmployeeById(String employeeId) {
+        for (int i = 0; i < size; i++) {
+            if (employeeId.equals(array[i].getID()) && !array[i].isActive()) {
+                array[i].setActive(true);
+                System.out.println("Status changed!");
+            } else if (employeeId.equals(array[i].getID()) && array[i].isActive()) {
+                System.out.println("Status is already true");
+                break;
+            } else {
+                System.out.println("Wrong id please try again");
+                return;
+            }
+        }
+    }
+}
